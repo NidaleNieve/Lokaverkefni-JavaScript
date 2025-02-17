18a1f9e3a6f30083c1e67826d3d6bcb108915baf
NidaleNieve <nidale16@outlook.com>
2025-02-17 13:28:05 +0000

    Bætti við fyrsta eventinu, baby steps

---
+++ main.js
@@ -0,0 +1,28 @@
+[
+    {
+        "name": "Vetrarhátíð",
+        "description": "Þessi hátíð ljóss og myrkurs samstendur af þrem meginstoðum: Safnanótt, Sundlauganótt og ljósalist ásamt yfir 150 viðburðum þar sem fjöldi listamanna tekur þátt í að skapa einstaka stemningu í borginni.",
+        "events": ["list", "tónlist", "tónleikar", "sýningar", "bókmenntir", "bækur", "dans", "Fjölskylda", "myndlist", "sviðslist", "fjölmenning"],
+        "imageUrl": "https://reykjavik.is/sites/default/files/styles/super_hero_4k/public/2024-02/vetrarhatid_2025_vefur_fb_bannerar-01.jpg?h=94f697f0&itok=EbT0zAPk",
+        "date": "2024-02-06",
+        "location": {
+            "city": "Reykjavik",
+            "latitude": 64.1334671,
+            "longitude": -21.9348417,
+        },
+        "url": "https://reykjavik.is/vetrarhatid"
+    },
+    {
+        "name": "",
+        "description": "",
+        "events": ["list", "tónlist", "tónleikar", "sýningar", "bókmenntir", "bækur", "dans", "Fjölskylda", "myndlist", "sviðslist", "fjölmenning"],
+        "imageUrl": "",
+        "date": "",
+        "location": {
+            "city": "",
+            "latitude": ,
+            "longitude": ,
+        },
+        "url": ""
+    },
+]
